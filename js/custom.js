5bfe9fa7999b277033457ea92375e7dcf387a4ab
hahglmht123 <103878643+hahglmht123@users.noreply.github.com>
2022-04-17 18:29:19 +0700

    porfolioluongvuha

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+new WOW().init();
+var typed = new Typed('.element', {
+    strings: ["Web Designer.", "Code Developer.","Apps Designer."],
+typeSpeed:100,
+    backSpeed:100,
+    loop:true,
+  });
